5b06109db04b788864c285652040baf8dd27b816
tarrantrl <tarrantrl@utexas.edu>
2020-11-01 21:52:11 -0600

    modularize server.js file

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,7 @@
+-- DROP TABLE IF EXISTS parties;
+-- DROP TABLE IF EXISTS candidates;
+-- DROP TABLE IF EXISTS voters;
+
 CREATE TABLE parties (
     id INTEGER PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
@@ -14,6 +18,14 @@ CREATE TABLE candidates (
     party_id INTEGER UNSIGNED,
     CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
+
+CREATE TABLE voters (
+    id INTEGER PRIMARY KEY ,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+)
 
 -- .mode column
 -- .headers on
