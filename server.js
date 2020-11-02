ff27f44fe96f43d5b02cdd301dd8ff3053a90d9e
tarrantrl <tarrantrl@utexas.edu>
2020-11-01 21:28:26 -0600

    add api requests for parties and update party affiliation

--- server.js
+++ server.js
@@ -39,6 +39,22 @@ app.get('/api/candidates', (req, res) => {
     });
 });
 
+// get all parties
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({error:err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
 // get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
     const sql = `SELECT candidates.*, parties.name 
@@ -61,6 +77,22 @@ app.get('/api/candidate/:id', (req, res) => {
     });
 });
 
+// get a single party
+app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({error:err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        })
+    })
+})
+
 // delete a candidate
 app.delete('/api/candidate/:id', (req, res) => {
     const sql = `DELETE FROM candidates WHERE id = ?`;
@@ -77,6 +109,19 @@ app.delete('/api/candidate/:id', (req, res) => {
         });
     });
 });
+
+// delete a party
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+        if (err){
+            res.status(400).json({error:res.message});
+            return;
+        }
+        res.json({message:'successfully deleted', changes: this.changes});
+    })
+})
 
 // create a candidate
 app.post('/api/candidate', ({body}, res) => {
@@ -96,6 +141,28 @@ app.post('/api/candidate', ({body}, res) => {
             message: 'success',
             data: body,
             id: this.lastID
+        })
+    })
+})
+
+// update a candidate's party affiliation
+app.put('/api/candidate/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'party_id');
+    if (errors){
+        res.status(400).json({error: errors});
+        return;
+    }
+    const sql = `UPDATE candidates SET party_id = ? WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+    db.run(sql, params, function(err, result){
+        if (err){
+            res.status(400).json({error:err.message});
+            return;
+        }
+        res.json({
+            message: "success",
+            data: req.body,
+            changes: this.changes
         })
     })
 })
