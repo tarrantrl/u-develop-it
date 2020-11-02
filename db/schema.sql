7da403580eed0640de7a399bd529d87d1a8651fc
tarrantrl <tarrantrl@utexas.edu>
2020-11-01 21:15:07 -0600

    add party info to get requests using left join

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,20 @@
-CREATE TABLE candidates (
-    id INTEGER PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    industry_connected BOOLEAN NOT NULL
-);
 CREATE TABLE parties (
     id INTEGER PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     description TEXT
-)
+);
+
+-- parties tables must be created first because foreign key relies on it
+
+CREATE TABLE candidates (
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    industry_connected BOOLEAN NOT NULL,
+    party_id INTEGER UNSIGNED,
+    CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
+
+-- .mode column
+-- .headers on
+-- PRAGMA foreign_keys = ON
