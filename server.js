4b854692e0dbcd20c8aff6f376101e699c51b4e7
tarrantrl <tarrantrl@utexas.edu>
2020-11-01 19:13:15 -0600

    add database connection in server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,14 @@ const app = express();
 // middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+// connect to db
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err){
+        return console.error(err.message);
+    }
+    console.log('Connected to the election db.');
+});
 
 app.get('/', (req, res) => {
     res.json({
@@ -17,6 +26,9 @@ app.use((req, res)=> {
     res.status(404).end();
 })
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Start server after db connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
